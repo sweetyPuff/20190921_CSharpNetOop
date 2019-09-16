f4fe2aae5ef3878b27f6a5800ab30cac995f287d
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2019-09-16 22:23:53 +0800

    complete object compare

---
+++ CCharpNetOop/MagicObjects.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CCharpNetOop
+{
+	internal class MagicObjects
+	{
+		public void MagicObject(int i)
+		{
+			object a = i;
+			object b = i;
+
+			Console.WriteLine(nameof(MagicObject)+nameof(i));
+			Console.WriteLine(a == b);
+			Console.WriteLine(a.Equals(b));
+
+		}
+
+		public void MagicObject()
+		{
+			object a = "a";
+			object b = "a";
+
+			Console.WriteLine(nameof(MagicObject));
+			Console.WriteLine(a == b);
+			Console.WriteLine(a.Equals(b));
+
+		}
+
+		public void MagicObject(string s)
+		{
+			object a = s;
+			object b = s;
+			a += "a";
+			b += "a";
+
+			Console.WriteLine(nameof(MagicObject)+nameof(s));
+			Console.WriteLine(a == b);
+			Console.WriteLine(a.Equals(b));
+
+		}
+	}
+}
--- CCharpNetOop/Program.cs
+++ CCharpNetOop/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using TypesTrial;
 
 namespace CCharpNetOop
 {
@@ -7,20 +6,10 @@ namespace CCharpNetOop
 	{
 		private static void Main(string[] args)
 		{
-			var employee = new EmployeeClass();
-
-			employee.Name ="Kyo1";
-			employee.Salary = 101;
-
-			Console.WriteLine($"Name={employee.Name}.Salary={employee.Salary}");
-
-			var employeeStruct = new EmployeeStruct();
-
-			employeeStruct.Name = "Kyo2";
-			employeeStruct.Salary = 102;
-
-			Console.WriteLine($"Name={employeeStruct.Name}.Salary={employeeStruct.Salary}");
-
+			var mo = new MagicObjects();
+			mo.MagicObject();
+			mo.MagicObject(1);
+			mo.MagicObject("a");
 			Console.ReadLine();
 		}
 	}
