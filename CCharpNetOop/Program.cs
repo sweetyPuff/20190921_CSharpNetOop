6d657830cfe63c7a40e314fdc52bf03035fe4bee
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2019-09-16 22:49:00 +0800

    Add string

--- CCharpNetOop/Program.cs
+++ CCharpNetOop/Program.cs
@@ -6,18 +6,41 @@ namespace CCharpNetOop
 	{
 		private static void Main(string[] args)
 		{
+			Console.WriteLine("object:");
 			var mo = new MagicObjects();
 
 			mo.MagicObject();
 			mo.MagicObject(1);
 			mo.MagicObject("a");
 
+			Console.WriteLine();
+			Console.WriteLine("dynamic:");
 			dynamic d = "d";
 			d += 1;
 			d += "123";
 			Console.WriteLine(d);
 
+			Console.WriteLine();
+			Console.WriteLine("string:");
+			var a= new MyStringClass {StringName = "Hello"};
+			var b = a;
+			b.StringName = "World";
+
+			Console.WriteLine(a.StringName);
+			Console.WriteLine(b.StringName);
+
+			string strA = "hello";
+			string strB = strA;
+			strB = "world";
+			Console.WriteLine(strA);
+			Console.WriteLine(strB);
+
 			Console.ReadLine();
 		}
+	}
+
+	internal class MyStringClass
+	{
+		public string StringName { get; set; }
 	}
 }
