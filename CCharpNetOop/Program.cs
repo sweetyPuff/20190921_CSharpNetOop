ba56a4eff9baa00bd5072ccdc68395c0497456fb
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2019-09-16 22:32:08 +0800

    dynamic

--- CCharpNetOop/Program.cs
+++ CCharpNetOop/Program.cs
@@ -7,9 +7,16 @@ namespace CCharpNetOop
 		private static void Main(string[] args)
 		{
 			var mo = new MagicObjects();
+
 			mo.MagicObject();
 			mo.MagicObject(1);
 			mo.MagicObject("a");
+
+			dynamic d = "d";
+			d += 1;
+			d += "123";
+			Console.WriteLine(d);
+
 			Console.ReadLine();
 		}
 	}
