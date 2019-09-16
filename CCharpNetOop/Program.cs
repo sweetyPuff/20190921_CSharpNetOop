7054e41be6a2ba40dbb73919a488ae66bcb11f93
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2019-09-16 23:48:55 +0800

    simple high couple oberver

--- CCharpNetOop/Program.cs
+++ CCharpNetOop/Program.cs
@@ -10,6 +10,41 @@ namespace CCharpNetOop
 	{
 		static void Main(string[] args)
 		{
+			var ea = new EaClass();
+			ea.Attach(new Employee(){Name="Kyo", Ea=ea});
+			ea.Attach(new Employee(){Name="Amanda", Ea = ea});
+			ea.Situation = "DM is back, ready to work!!";
+			ea.Notify();
+		}
+	}
+
+	internal class Employee
+	{
+		public string Name { get; set; }
+		public EaClass Ea { get; set; }
+
+		public void Update()
+		{
+			Console.WriteLine($"{Name}!! {Ea.Situation}!!");
+		}
+	}
+
+	internal class EaClass
+	{
+		private List<Employee> _employees = new List<Employee>();
+		public string Situation { get; set; }
+
+		public void Attach(Employee employee)
+		{
+			_employees.Add(employee);
+		}
+
+		public void Notify()
+		{
+			foreach (var employee in _employees)
+			{
+				employee.Update();
+			}
 		}
 	}
 }
