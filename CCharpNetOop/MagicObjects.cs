970a963d10c57da41a7464d8f602c0c170643455
Lucy Kuo Ting-Rung <lucy.kuo@titansoft.com.sg>
2019-09-21 12:01:19 +0800

    Add comment

--- CCharpNetOop/MagicObjects.cs
+++ CCharpNetOop/MagicObjects.cs
@@ -11,18 +11,18 @@ namespace CCharpNetOop
 
 			Console.WriteLine(nameof(MagicObject)+nameof(i));
 			Console.WriteLine(a == b);
-			Console.WriteLine(a.Equals(b));
+			Console.WriteLine(a.Equals(b)); // 使用int的Equals, 但object的.Equals是比對記憶體位置的
 
 		}
 
 		public void MagicObject()
 		{
 			object a = "a";
-			object b = "a";
+			object b = "a"; // compile time 會做String最佳化 (run time不會做string最佳化), 指到同一個記憶體位置
 
 			Console.WriteLine(nameof(MagicObject));
 			Console.WriteLine(a == b);
-			Console.WriteLine(a.Equals(b));
+			Console.WriteLine(a.Equals(b)); // 使用string的Equals, 兩個都是比對記憶體位置
 
 		}
 
@@ -30,12 +30,12 @@ namespace CCharpNetOop
 		{
 			object a = s;
 			object b = s;
-			a += "a";
-			b += "a";
+			a += "a"; // 各自產生新的記憶體位置
+			b += "a"; // 各自產生新的記憶體位置 += 的運算時是run time, 不會做string 最佳化
 
-			Console.WriteLine(nameof(MagicObject)+nameof(s));
-			Console.WriteLine(a == b);
-			Console.WriteLine(a.Equals(b));
+            Console.WriteLine(nameof(MagicObject)+nameof(s));
+			Console.WriteLine(a == b); // 判斷記憶體位置是否相同
+			Console.WriteLine(a.Equals(b)); // 判斷value是否相同
 
 		}
 	}
--- CCharpNetOop/Program.cs
+++ CCharpNetOop/Program.cs
@@ -14,7 +14,7 @@ namespace CCharpNetOop
 			mo.MagicObject("a");
 
 			Console.WriteLine();
-			Console.WriteLine("dynamic:");
+			Console.WriteLine("dynamic:"); //不檢查型別, 一切都在run time決定 compile時感覺一片祥和
 			dynamic d = "d";
 			d += 1;
 			d += "123";
@@ -24,7 +24,7 @@ namespace CCharpNetOop
 			Console.WriteLine("string:");
 			var a= new MyStringClass {StringName = "Hello"};
 			var b = a;
-			b.StringName = "World";
+			b.StringName = "World"; // b是ref到a, 把b裡面的string name property改掉了, 但a/b都是指到同一個instance, 所以一起被改掉了 
 
 			Console.WriteLine(a.StringName);
 			Console.WriteLine(b.StringName);
@@ -32,6 +32,7 @@ namespace CCharpNetOop
 			string strA = "hello";
 			string strB = strA;
 			strB = "world";
+            string cccc = new string('a', 3);
 			Console.WriteLine(strA);
 			Console.WriteLine(strB);
 
