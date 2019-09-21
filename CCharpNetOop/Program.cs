6b93be2aa28470d60ad82682dafb0b25babacb86
Lucy Kuo Ting-Rung <lucy.kuo@titansoft.com.sg>
2019-09-21 21:48:41 +0800

    Add caculator

--- CCharpNetOop/Program.cs
+++ CCharpNetOop/Program.cs
@@ -10,6 +10,53 @@ namespace CCharpNetOop
 	{
 		static void Main(string[] args)
 		{
+		    Console.WriteLine("chose method");
+		    var method = Console.ReadLine();
+		    Console.WriteLine("input first number");
+		    var number1 = int.Parse(Console.ReadLine());
+		    Console.WriteLine("input second number");
+		    var number2 = int.Parse(Console.ReadLine());
+
+		    var operators = new Dictionary<string, IOperator>()
+		    {
+		        {"+", new AddOperator()} ,
+		        {"-", new MinusOperator()} ,
+            };
+
+		    var result = 0;
+		    switch (method)
+		    {
+		        case "+":
+		            result = operators["+"].Process(number1, number2);
+                    break;
+		        case "-":
+		            result = operators["-"].Process(number1, number2);
+                    break;
+		    }
+
+		    Console.WriteLine(result);
+		    Console.Read();
 		}
 	}
+
+    public interface IOperator
+    {
+        int Process(int n1, int n2);
+    }
+
+    public class AddOperator : IOperator
+    {
+        public int Process(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+
+    public class MinusOperator : IOperator
+    {
+        public int Process(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+    }
 }
